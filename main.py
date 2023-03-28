5e478ebb2af9f56d0eb473a1049f7fde3c454001
JimmyLi000 <102612222+JimmyLi000@users.noreply.github.com>
2023-03-29 00:12:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+# 引入必要的套件
 from flask import Flask, request, abort
 
+# 建立 Flask 應用程式物件
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/")     # 函式的裝飾 , /是要處理的路徑
+def hello_world():  #定義一個fucntion叫hello_world()
     html = f"<h1>Deployed with Zeet!!</h1>"
     return html
 
-if __name__ == '__main__':
-	app.run(host="0.0.0.0", port=3000)
+if __name__ == '__main__':      # 如果執行主程式
+	app.run(host="0.0.0.0", port=3000)   # 立刻啟動伺服器
